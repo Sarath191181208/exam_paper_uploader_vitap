d43e4fc1fac89f0ea96042d7e1e0865514bded27
Sarath <74459981+Sarath191181208@users.noreply.github.com>
2024-09-12 01:11:48 +0000

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'],
     },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ['ubiquitous-space-guide-v5vg7qqxv9v2964-3000.app.github.dev', 'localhost:3000'],
+        }
+    }
 };
 
 export default nextConfig;
