716c4d23e06fa26e0b44dd9a2ec4096b1742a52f
Sarath <74459981+Sarath191181208@users.noreply.github.com>
2024-09-12 01:22:19 +0000

    +extracted validator into a new file

--- app/hooks/uploadPaperForm.tsx
+++ app/hooks/uploadPaperForm.tsx
@@ -1,5 +1,5 @@
-import { error } from "console";
 import { useState } from "react";
+import { validateUploadForm } from "app/validators/validateUpload";
 
 export interface FormState {
   name: string;
@@ -29,23 +29,8 @@ export function useUploadPaperForm() {
   const [formState, setFormState] = useState<FormState>(getEmptyFormState());
   const [errors, setErrors] = useState<FormErrors>({});
   const validateForm = () => {
-    const newErrors: FormErrors = {};
-    if (!formState.name) newErrors.name = "Name is required.";
-    if (!formState.courseCode) {
-      newErrors.courseCode = "Course Code is required.";
-    } else if (!/\w{3}\d{4}/.test(formState.courseCode)) {
-      newErrors.courseCode = 'Course code should be in the format "CSE1004".';
-    }
-    if (!formState.examType) {
-      newErrors.examType = "Type of the Exam is required.";
-    }
-    if (!formState.examSlot) newErrors.examSlot = "Exam Slot is required.";
-    if (formState.examDate == undefined) newErrors.examDate = "Date is required";
-    else {
-      if (formState.examDate > new Date()) newErrors.examDate = "Date shouldn't be in the future.";
-    }
+    const newErrors: FormErrors = validateUploadForm(formState)
     setErrors(newErrors);
-
     return Object.keys(newErrors).length === 0;
   };
   const [images, setImages] = useState<string[]>([]);
---
+++ app/validators/validateUpload.ts
@@ -0,0 +1,32 @@
+import { FormErrors, FormState } from "app/hooks/uploadPaperForm";
+
+export function validateUploadForm(formState: FormState): FormErrors {
+    const newErrors: FormErrors = {};
+    const COURSE_CODE_REGEX = /\w{3}\d{4}/;
+
+    // validate name errors
+    if (!formState.name) newErrors.name = "Name is required.";
+
+    // check required course code and format
+    if (!formState.courseCode) {
+        newErrors.courseCode = "Course Code is required.";
+    } else if (!COURSE_CODE_REGEX.test(formState.courseCode)) {
+        newErrors.courseCode = 'Course code should be in the format "CSE1004".';
+    }
+
+    // check required exam type
+    if (!formState.examType) {
+        newErrors.examType = "Type of the Exam is required.";
+    }
+
+    // check required exam slot
+    if (!formState.examSlot) newErrors.examSlot = "Exam Slot is required.";
+
+    // check required exam date and if inputed future date
+    if (formState.examDate == undefined) newErrors.examDate = "Date is required";
+    else {
+        if (formState.examDate > new Date()) newErrors.examDate = "Date shouldn't be in the future.";
+    }
+
+    return newErrors;
+}
