7f4e4ee701a7ed479ddde1868a1c1a5422e59c36
Sarath191181208 <vssarathc04@gmail.com>
2024-10-11 21:36:23 +0530

    +use the new firebase path util

--- app/upload/uploadImage.ts
+++ app/upload/uploadImage.ts
@@ -1,11 +1,11 @@
 import {
-  getFirestore,
   runTransaction,
   doc,
   increment,
 } from "firebase/firestore";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { firestore, storage } from "app/firebase/firebase";
+import { getStorageImageUploadPath, getUserUploadDocPath } from "app/firebase/paths";
 
 export async function uploadImages(
   userId: string,
@@ -13,7 +13,7 @@ export async function uploadImages(
   courseCode: string
 ): Promise<string[]> {
   // Reference to the Firestore document for the user's upload count
-  const userUploadRef = doc(firestore, `/users/uploads/${userId}`);
+  const userUploadRef = doc(firestore, getUserUploadDocPath(userId));
 
   // Start a Firestore transaction
   const downloadURLs: string[] = await runTransaction(
@@ -29,26 +29,22 @@ export async function uploadImages(
 
       // Check if the user has exceeded the upload limit
       const totalUploads = numberOfUploads + images.length;
-      if (totalUploads > 1000) {
+      if (totalUploads > 150) {
         throw new Error(
-          "Upload limit exceeded. You can only upload up to 1000 images."
+          "Upload limit exceeded. You can only upload up to 150 images."
         );
       }
-
-      // Increment the numberOfUploads
-      transaction.update(userUploadRef, {
-        numberOfUploads: increment(images.length),
-      });
 
       // Upload the images in parallel and store their download URLs
       const uploadPromises = images.map(async (image, i) => {
         // Extract the original file extension from the image
         const fileExtension = image.name.split(".").pop(); // e.g., 'jpg', 'png'
+        const fileName = `${i}.${fileExtension}`;
 
         // Create a reference to the storage location using the index as the file name
         const storageRef = ref(
           storage,
-          `${userId}/${courseCode}/${i}.${fileExtension}`
+          getStorageImageUploadPath(userId, fileName)
         );
 
         const uploadResult = await uploadBytes(storageRef, image);
@@ -56,6 +52,11 @@ export async function uploadImages(
       });
 
       const downloadURLs = await Promise.all(uploadPromises);
+
+      // Increment the numberOfUploads
+      transaction.set(userUploadRef, {
+        numberOfUploads: increment(images.length),
+      }, {merge: true});
       return downloadURLs;
     }
   );
