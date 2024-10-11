5ff76bd2952d58044e51d1530cb72e37c990c499
Sarath191181208 <vssarathc04@gmail.com>
2024-10-11 21:34:34 +0530

    +errors

--- app/actions/actionStates.ts
+++ app/actions/actionStates.ts
@@ -3,4 +3,6 @@ export const uploadActionStates = {
   noData : "No data provided",
   validationError: "Validation failed",
   success: "Date uploaded sucessfully",
+  unknownError: "Unknown Error has occured",
+  invalidNumberofImages: "The images must be between 1 and 5"
 };
