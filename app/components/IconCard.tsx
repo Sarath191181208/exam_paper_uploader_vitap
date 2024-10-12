9b21ce80ebac79198adc6a23c61561132263edd6
Sarath191181208 <vssarathc04@gmail.com>
2024-10-12 12:40:58 +0530

    +onclick to iconcard

--- app/components/IconCard.tsx
+++ app/components/IconCard.tsx
@@ -37,11 +37,12 @@ export const IconCardDescription: React.FC<IconCardDescriptionProps> = (
 
 interface IconCardProps {
   children: React.ReactNode;
+  onClick: () => void
 }
 
-export const IconCard: React.FC<IconCardProps> = ({ children }) => {
+export const IconCard: React.FC<IconCardProps> = ({ children, onClick}) => {
   return (
-    <div className="bg-gray-800 rounded-lg shadow-lg p-6 hover:scale-105 transition-all flex flex-col items-center text-center">
+    <div onClick={onClick} className="bg-gray-800 rounded-lg shadow-lg p-6 hover:scale-105 transition-all flex flex-col items-center text-center">
       {children}
     </div>
   );
