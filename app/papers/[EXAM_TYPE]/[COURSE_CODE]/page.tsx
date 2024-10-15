cd99843a0822fa6655fb5b87483ed7670f7c8edf
Sarath191181208 <vssarathc04@gmail.com>
2024-10-16 01:18:53 +0530

    rm imgs from page

--- app/papers/[EXAM_TYPE]/[COURSE_CODE]/page.tsx
+++ app/papers/[EXAM_TYPE]/[COURSE_CODE]/page.tsx
@@ -1,7 +1,6 @@
 import ExamCard from "@/app/components/ExamCard";
 import { CourseCode, ExamEntry, ExamType } from "@/app/data/ExamEntry";
 import { fetchExamEntries } from "@/app/firebase/fetchData";
-import Image from "next/image";
 import React from "react";
 
 export default async function Home(
@@ -18,25 +17,6 @@ export default async function Home(
         {Object.values(examData).map((data: ExamEntry, index: number) => (
           <div>
             <ExamCard key={index} {...data} />
-            <div className="flex gap-4 mt-4 overflow-x-scroll overflow-y-hidden">
-              {data.imageURLs.map((url, idx: number) => (
-               <a href={url} target="_blank" rel="noopener noreferrer">
-                <div
-                  key={index}
-                  className="relative w-full h-52 bg-transparent flex items-center justify-center rounded-lg"
-                >
-                  <Image
-                    key={idx}
-                    src={url}
-                    alt={`Image ${idx + 1} for ${data.name}`}
-                    width={1080} // Provide appropriate width
-                    height={720} // Provide appropriate height
-                    className="w-full min-w-[320px]"
-                  />
-                </div>
-              </a>
-              ))}
-            </div>
           </div>
         ))}
       </div>
