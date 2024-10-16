6fa31f487380b9c04282e81d10a2082f36ff06e3
Sarath191181208 <vssarathc04@gmail.com>
2024-10-16 10:36:16 +0530

    +loading for /upload route

---
+++ app/upload/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingCard from "app/components/LoadingCard";
+
+export default function LoadingPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <LoadingCard />
+    </div>
+  );
+}
