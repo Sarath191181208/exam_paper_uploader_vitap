6ea07b4c93d1a21a46e010a1854b01431a537671
Sarath191181208 <vssarathc04@gmail.com>
2024-10-16 16:45:17 +0530

    invalid init admin app fix

--- app/firebase/firebaseAdmin.ts
+++ app/firebase/firebaseAdmin.ts
@@ -1,14 +1,23 @@
-import { cert, initializeApp, ServiceAccount,  } from "firebase-admin/app";
+import { App, cert, ServiceAccount } from "firebase-admin/app";
+import fadmin from 'firebase-admin';
 import { getFirestore } from "firebase-admin/firestore";
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
 const firebaseOptions: ServiceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: privateKey,
 };
 
-export const admin = initializeApp({
-  credential: cert(firebaseOptions),
-}, process.env.FIREBASE_PROJECT_ID);
+function getAdmin(): App {
+  if (fadmin.apps.length === 0) {
+    return fadmin.initializeApp({
+      credential: cert(firebaseOptions),
+    });
+  }
+  return fadmin.app();
+}
 
+export const admin = getAdmin();
 export const firestoreAdmin = getFirestore(admin);
